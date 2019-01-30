7cde211a6d13b20e71691cd7f7b5b00d1820e580
caio sanchez <caioschristino@gmail.com>
2019-01-30 17:56:39 -0200

    update file

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var https = require('https');
 
 var argv = minimist(process.argv.slice(2), {
     default: {
-        as_uri: 'https://client-cam.herokuapp.com',
+        as_uri: 'https://localhost:8443/',
         ws_uri: 'ws://localhost:8888/kurento'
     }
 });
